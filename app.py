de5a83b020ec8d6ff794c17bad4417d48d582b22
SHIVAM MALVIYA <145798546+shivuu2005@users.noreply.github.com>
2024-12-05 14:22:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,11 +10,12 @@ app = Flask(__name__)
 app.secret_key = "your_secret_key"
 
 # MySQL Configuration
-app.config["MYSQL_HOST"] = "localhost"
+app.config["MYSQL_HOST"] = "${{RAILWAY_PRIVATE_DOMAIN}}"
 app.config["MYSQL_USER"] = "root"
-app.config["MYSQL_PASSWORD"] = ""
-app.config["MYSQL_DB"] = "attendance_system"
+app.config["MYSQL_PASSWORD"] = "${{MYSQL_ROOT_PASSWORD}}"
+app.config["MYSQL_DB"] = "railway"
 mysql = MySQL(app)
+
 
 
 
@@ -589,4 +590,4 @@ def logout():
     return redirect(url_for("index"))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0', port=5000)
