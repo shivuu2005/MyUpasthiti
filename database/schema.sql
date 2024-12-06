987711506955f71d080f82bafb0d38dca33a8f6a
SHIVAM MALVIYA <145798546+shivuu2005@users.noreply.github.com>
2024-12-06 15:36:32 +0530

    Update schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -2,6 +2,12 @@ CREATE DATABASE attendance_system;
 
 USE attendance_system;
 
+DROP TABLE IF EXISTS `admin`;
+DROP TABLE IF EXISTS `attendance`;
+DROP TABLE IF EXISTS `users`;
+
+DROP TABLE IF EXISTS `reports`;
+
 -- User Table
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -32,4 +38,14 @@ CREATE TABLE admin (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
     password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE reports (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    enrollment_number VARCHAR(20) NOT NULL,
+    date DATE NOT NULL,
+    problem_type VARCHAR(50) NOT NULL,
+    message TEXT NOT NULL
 );
